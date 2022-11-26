8f01d0d006cd02b6e6d8189e9a6c8bcc45764f00
Nekobuttler <cristiaw86@gmail.com>
2022-11-26 01:23:35 -0600

    12 rst Week Commit a

--- app/src/main/java/com/example/places/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/places/ui/gallery/GalleryFragment.kt
@@ -8,32 +8,90 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.places.databinding.FragmentGalleryBinding
+import com.example.places.model.Place
 import com.example.places.viewmodel.GalleryViewModel
+import com.example.places.viewmodel.PlaceViewModel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class GalleryFragment : Fragment() {
+class GalleryFragment : Fragment() , OnMapReadyCallback {
 
     private var _binding: FragmentGalleryBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    //Este objeto sera para interactuar con el mapa de la vista
+
+    private lateinit var googleMap : GoogleMap
+
+    private var mapReady =  false
+
+    //Se toman lso datos de los lugares desde PlaceViewModel
+
+    private lateinit var placeViewModel: PlaceViewModel
+
+    //Funcion especial que se ejecuta al crear el Activity
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //Se solicita la actualizacion del mapa
+        //desplegando en la pantalla los lugares
+
+        binding.map.onCreate(savedInstanceState)
+        binding.map.onResume()
+        binding.map.getMapAsync (this@GalleryFragment)
+
+    }
+
+    //Cuando el mapa ya esta lista para mostrarse
+
+    override fun onMapReady(map: GoogleMap) {
+        map.let{
+            googleMap = it
+            mapReady = true
+
+            //Se instruye al mapa para que se acutalize la info de los lugares segun la vista
+
+            placeViewModel.getPlaces.observe(viewLifecycleOwner){
+                places ->
+                updateMap(places)
+
+            }
+
+        }
+    }
+
+    private fun updateMap(places: List<Place>?) {
+        if(mapReady) {
+            places?.forEach {
+            place ->
+                if(place.latitud?.isFinite() == true
+                    &&
+                    place.longitud?.isFinite() == true){
+                    val marca = LatLng(place.latitud,place.longitud)
+                    googleMap.addMarker(MarkerOptions().position(marca).title("${place.name}"))
+                }
+
+        }
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val placeViewModel =
+            ViewModelProvider(this)[PlaceViewModel :: class.java]
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+
+
+        return binding.root
     }
 
     override fun onDestroyView() {
